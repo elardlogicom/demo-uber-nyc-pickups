075c61d19c99c6eecb0a562178415bfbd4b662d5
ElliotLard <elliotlard@gmail.com>
2022-01-14 13:39:10 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,137 +1,123 @@
 # -*- coding: utf-8 -*-
-# Copyright 2018-2019 Streamlit Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+"""
+Created on Tue Nov 16 13:31:34 2021
 
-"""An example of showing geographic data."""
-
+@author: Elliot Lard
+"""
+import seaborn as sns
 import streamlit as st
 import pandas as pd
+from datetime import timedelta
 import numpy as np
-import altair as alt
-import pydeck as pdk
+import matplotlib
+import matplotlib.pyplot as plt
+import datetime
 
-# SETTING PAGE CONFIG TO WIDE MODE
-st.set_page_config(layout="wide")
 
-# LOADING DATA
-DATE_TIME = "date/time"
-DATA_URL = (
-    "http://s3-us-west-2.amazonaws.com/streamlit-demo-data/uber-raw-data-sep14.csv.gz"
-)
+def text_area(label):
+    final_comment.append(label)
+    inp = st.text_area(label)
+    final_comment.append(inp)
+    return inp
+def radio(label, ls):
+    final_comment.append(label)
+    inp = st.radio(label, ls)
+    final_comment.append(inp)
+    return inp
+def text_input(label):
+    final_comment.append(label)
+    inp = st.text_input(label)
+    final_comment.append(inp)
+    return inp
+def write(text):
+    final_comment.append(text)
+    final_comment.append('\n')
+    st.write(text)
+st.write('<style>div.row-widget.stRadio > div{flex-direction:row;}</style>', unsafe_allow_html=True)
+up_down = ('Up', 'Down')
+yes_no = ('Yes', 'No')
+gro = ('Green', 'Red', 'Off')
 
-@st.cache(persist=True)
-def load_data(nrows):
-    data = pd.read_csv(DATA_URL, nrows=nrows)
-    lowercase = lambda x: str(x).lower()
-    data.rename(lowercase, axis="columns", inplace=True)
-    data[DATE_TIME] = pd.to_datetime(data[DATE_TIME])
-    return data
 
-data = load_data(100000)
+with st.sidebar.expander('select ONT info'):
+    ont_fiber_aoe = st.selectbox('ONT Fiber', up_down)
+    ont_lan_aoe = st.selectbox('ONT LAN', up_down)
+    uptime = st.number_input('ONT Uptime', 0, step=1)
+    service_status = st.selectbox('Service Status', ['Active', 'Suspended'])
+    upstream_BIP = st.number_input('Upstream BIP', 0, step=1)
+    downstream_BIP = st.number_input('Downstream BIP', 0, step=1)
+    ip = st.text_input('ip address', '192.168.1.1')
+    olt_rx = st.number_input('OLT RX Power (RSSI) dBm', -17.2, step = 0.1)
+    ont_rx = st.number_input('ONT RX Power (dBm)', -18.2, step = 0.1)
+    ont_tx = st.number_input('ONT TX Power (dBm)', 1.5, step = 0.1)
+with st.sidebar.expander('ONT Info'):
+    st.write('Ticket No. 145828')
+    st.write('Name: John Doe')
+    st.write('Callback Number: 8188889023')
+    st.write('Account Number: 5687413')
+    st.write('Service Address: 21 Jump St')
+    st.write('ONT fiber: ' + ont_fiber_aoe)
+    st.write('ONT LAN: ' + ont_lan_aoe)
+    st.write('ONT Uptime: ' + str(uptime))
+    st.write('Service Status: ' + service_status)
+    st.write('Service Package: Res 300mbps')
+    st.write('IP Address: ' + ip)
+    st.write('Upstream BIP: ' + str(upstream_BIP))
+    st.write('Downstream BIP: ' + str(downstream_BIP))
+    st.write('OLT RX Power (RSSI) dBm: ' + str(olt_rx))
+    st.write('ONT RX Power (dBm): ' + str(ont_rx))
+    st.write('ONT TX Power (dBm): ' + str(ont_tx))
+    st.button('Paste to Commnet')
+    st.button('Refresh')
+    
+issues = ['(QOS) Slow Internet Troubleshooting', '(LOS) No Internet All Devices', 'Intermittent Internet', 'Slow Speeds', 'Customer Service', 'Member Equipment', 'Voice', 'Video']
 
-# CREATING FUNCTION FOR MAPS
+final_comment = []
 
-def map(data, lat, lon, zoom):
-    st.write(pdk.Deck(
-        map_style="mapbox://styles/mapbox/light-v9",
-        initial_view_state={
-            "latitude": lat,
-            "longitude": lon,
-            "zoom": zoom,
-            "pitch": 50,
-        },
-        layers=[
-            pdk.Layer(
-                "HexagonLayer",
-                data=data,
-                get_position=["lon", "lat"],
-                radius=100,
-                elevation_scale=4,
-                elevation_range=[0, 1000],
-                pickable=True,
-                extruded=True,
-            ),
-        ]
-    ))
 
-# LAYING OUT THE TOP SECTION OF THE APP
-row1_1, row1_2 = st.columns((2,3))
+text_area('Member Description of Issue')
 
-with row1_1:
-    st.title("NYC Uber Ridesharing Data")
-    hour_selected = st.slider("Select hour of pickup", 0, 23)
+member_issue = st.selectbox("Member Issue", issues)
 
-with row1_2:
-    st.write(
-    """
-    ##
-    Examining how Uber pickups vary over time in New York City's and at its major regional airports.
-    By sliding the slider on the left you can view different slices of time and explore different transportation trends.
-    """)
+if member_issue == '(LOS) No Internet All Devices':
+    radio('ONT Status:', ['Up', 'Discovering'])
+    text_input('ONT Uptime:')
+    text_input('Device Location:')
+    
+    st.write('Reboot ONT and RG [wiki](http://xwiki.logicom.center:8080/xwiki/bin/view/T1/All%20COOPS/Proper%20ONTRG%20Reboot%20Sequence%20%28All%20COOPs%20except%20NAEC%29/)')
+    
+    write('ONT Status Before Reboot:')
+    cols = st.columns(3)
+    with cols[0]: radio('ONT Power Before Reboot:', gro)
+    with cols[1]: radio('ONT Fiber Before Reboot:', gro)
+    with cols[2]: radio('ONT LAN Before Reboot:', gro)
+    
+    write('Cabling:')
 
-# FILTERING DATA BY HOUR SELECTED
-data = data[data[DATE_TIME].dt.hour == hour_selected]
+    st.checkbox('Checked Cabling')
+    st.checkbox('Reboot ONT:')
+    st.checkbox('Reboot RG:')
+    # cols = st.columns(3)
+    # with cols[0]: radio('Checked Cabling:', yes_no)
+    # with cols[1]: ont_reboot = radio('Reboot ONT:', yes_no)
+    # with cols[2]: radio('Reboot RG:', yes_no)
+    
+    # if(ont_reboot=='Yes'):
+    #     write('ONT Status After Reboot:')
+    #     cols = st.columns(3)
+    #     with cols[0]: radio('ONT Power After Reboot:', gro)
+    #     with cols[1]: radio('ONT Fiber After Reboot:', gro)
+    #     with cols[2]: radio('ONT LAN After Reboot:', gro)
+    text_area('Result:')
 
-# LAYING OUT THE MIDDLE SECTION OF THE APP WITH THE MAPS
-row2_1, row2_2, row2_3, row2_4 = st.columns((2,1,1,1))
+with st.expander('Resolution Box'):
 
-# SETTING THE ZOOM LOCATIONS FOR THE AIRPORTS
-la_guardia= [40.7900, -73.8700]
-jfk = [40.6650, -73.7821]
-newark = [40.7090, -74.1805]
-zoom_level = 12
-midpoint = (np.average(data["lat"]), np.average(data["lon"]))
+    col = st.columns(2)
+    with col[0]:st.selectbox('Issue Code', ['CS customer service', 'QOS - speed', 'LOS - ONT fiber', 'LOS - damaged fiber'])
+    with col[1]: t1_res = st.selectbox('Tier 1 Resolution', ['escalation', 'TS - reboot ONT/RG', 'RG - 2.4ghz/5ghz separation/education'])
+    if t1_res == 'escalation': st.selectbox('escalation', ['External Standard - Internet', 'Internal Internet'])
+   
+    final_notes = '\n'.join(final_comment)
 
-with row2_1:
-    st.write("**All New York City from %i:00 and %i:00**" % (hour_selected, (hour_selected + 1) % 24))
-    map(data, midpoint[0], midpoint[1], 11)
-
-with row2_2:
-    st.write("**La Guardia Airport**")
-    map(data, la_guardia[0],la_guardia[1], zoom_level)
-
-with row2_3:
-    st.write("**JFK Airport**")
-    map(data, jfk[0],jfk[1], zoom_level)
-
-with row2_4:
-    st.write("**Newark Airport**")
-    map(data, newark[0],newark[1], zoom_level)
-
-# FILTERING DATA FOR THE HISTOGRAM
-filtered = data[
-    (data[DATE_TIME].dt.hour >= hour_selected) & (data[DATE_TIME].dt.hour < (hour_selected + 1))
-    ]
-
-hist = np.histogram(filtered[DATE_TIME].dt.minute, bins=60, range=(0, 60))[0]
-
-chart_data = pd.DataFrame({"minute": range(60), "pickups": hist})
-
-# LAYING OUT THE HISTOGRAM SECTION
-
-st.write("")
-
-st.write("**Breakdown of rides per minute between %i:00 and %i:00**" % (hour_selected, (hour_selected + 1) % 24))
-
-st.altair_chart(alt.Chart(chart_data)
-    .mark_area(
-        interpolate='step-after',
-    ).encode(
-        x=alt.X("minute:Q", scale=alt.Scale(nice=False)),
-        y=alt.Y("pickups:Q"),
-        tooltip=['minute', 'pickups']
-    ).configure_mark(
-        opacity=0.2,
-        color='red'
-    ), use_container_width=True)
+    st.text_area('Final Commnet', final_notes,height = 300)
+    st.button('submit')
